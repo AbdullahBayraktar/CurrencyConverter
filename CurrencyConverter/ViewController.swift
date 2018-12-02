605717a1dd22ad18851dc4f80e96a748a12ce641
Abdullah Bayraktar <abdullahbayraktar9@gmail.com>
2018-12-02 16:59:23 +0300

    Create Xcode Project

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Abdullah Bayraktar on 02/12/2018.
+//  Copyright © 2018 AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO: To be implemented
+    }
+}
+
