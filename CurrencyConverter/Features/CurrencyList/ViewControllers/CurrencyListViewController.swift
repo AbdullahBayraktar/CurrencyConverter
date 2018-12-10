4d4d44dd10d5c84b13761857a7c8514d5725429b
Abdullah Bayraktar <abdullahbayraktar9@gmail.com>
2018-12-10 03:14:03 +0300

    Implement Currencies List Feature

--- CurrencyConverter/Features/CurrencyList/ViewControllers/CurrencyListViewController.swift
+++ CurrencyConverter/Features/CurrencyList/ViewControllers/CurrencyListViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class CurrencyListViewController: UIViewController {
+final class CurrencyListViewController: UIViewController {
+
+    private enum Constant {
+        static let currenciesUpdatePeriod = 1.0
+    }
 
     /// Outlets
     @IBOutlet weak private var tableView: UITableView!
@@ -16,16 +20,55 @@ class CurrencyListViewController: UIViewController {
     /// View model
     var viewModel: CurrencyListViewModel!
 
+    /// Service Timer
+    var serviceTimer: Timer?
+
+    // MARK: - Factory
+
+    func initialize() {
+
+        let networkManager = NetworkManager()
+        let dataController = CurrenciesDataController(networkManager: networkManager)
+        viewModel = CurrencyListViewModel(with: dataController)
+    }
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        initialize()
         prepareViews()
-        // TODO: To be implemented
+        bindViewModel()
+
+        scheduledTimer()
+        viewModel.fetchCurrencyRates(isInitialFetch: true)
+    }
+
+    deinit {
+        serviceTimer?.invalidate()
+    }
+
+    // MARK: - Fetch Data
+
+    @objc func fetchCurrencyRates() {
+        viewModel.fetchCurrencyRates(isInitialFetch: false)
     }
 }
 
+// MARK: Timer
+
+private extension CurrencyListViewController {
+
+    func scheduledTimer() {
+        serviceTimer = Timer.scheduledTimer(
+            timeInterval: Constant.currenciesUpdatePeriod,
+            target: self,
+            selector: #selector(fetchCurrencyRates),
+            userInfo: nil,
+            repeats: true)
+    }
+}
 // MARK: - Views
 
 private extension CurrencyListViewController {
@@ -36,20 +79,52 @@ private extension CurrencyListViewController {
     }
 }
 
+// MARK: - Change Handler
+
+private extension CurrencyListViewController {
+
+    func bindViewModel() {
+        viewModel.stateChangeHandler = { [unowned self] change in
+            self.applyStateChange(change)
+        }
+    }
+
+    func applyStateChange(_ change: CurrencyListState.Change) {
+
+        switch change {
+        case .initialFetch(let isInitialFetch):
+            if isInitialFetch {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        case .amountsUpdate(let isUpdating):
+            if !isUpdating {
+                updateCellsAmountExceptSelectedCell()
+            }
+        }
+    }
+}
+
 // MARK: - UITableViewDataSource
 
 extension CurrencyListViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        // FIXME: Get currencies count from view model
-        return 0
+        return viewModel.currenciesCount
     }
 
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CurrencyListTableViewCell.className, for: indexPath)
-        // TODO: To be implemented
+        let cell = tableView.dequeueReusableCell(withIdentifier: CurrencyListTableViewCell.className, for: indexPath) as! CurrencyListTableViewCell
+
+        guard let currency = viewModel.currency(at: indexPath.row) else {
+            return cell
+        }
+
+        cell.configureCell(currency: currency,
+                           delegate: self)
         return cell
     }
 }
@@ -61,6 +136,78 @@ extension CurrencyListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView,
                    didSelectRowAt indexPath: IndexPath) {
 
-        // TODO: Handle cell selection
+        let cell = tableView.cellForRow(at: indexPath) as? CurrencyListTableViewCell
+        cell?.makeFirstResponder()
+        cellSelected(at: indexPath)
+    }
+
+    func tableView(_ tableView: UITableView,
+                   didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as? CurrencyListTableViewCell
+        cell?.resignResponder()
+    }
+}
+
+// MARK: - TableView Helpers
+
+private extension CurrencyListViewController {
+
+    func cellSelected(at indexPath: IndexPath) {
+
+        viewModel.moveToTopOfCurrencies(from: indexPath.row)
+
+        tableView.beginUpdates()
+        let topIndexPath = IndexPath(row: 0, section: 0)
+        tableView.moveRow(at: indexPath, to: topIndexPath)
+        tableView.endUpdates()
+        tableView.scrollToRow(at: topIndexPath, at: .top, animated: false)
+    }
+}
+
+// MARK: - CurrencyListTableViewCellDelegate
+
+extension CurrencyListViewController: CurrencyListTableViewCellDelegate {
+
+    func currencyListTableViewCell(
+        _ sender: CurrencyListTableViewCell,
+        editingChanged text: String?) {
+
+        viewModel.updateAmount(
+            amount: text,
+            currencyCode:sender.currencyCodeLabel.text ?? "")
+    }
+
+    func currencyListTableViewCell(
+        _ sender: CurrencyListTableViewCell,
+        editingDidBegin text: String?) {
+        
+        viewModel.updateSelectedCurrency(
+            amount: text,
+            currencyCode: sender.currencyCodeLabel.text ?? "")
+    }
+}
+
+// MARK: - TableView Helpers
+
+extension CurrencyListViewController {
+
+    func updateCellsAmountExceptSelectedCell() {
+
+        DispatchQueue.main.async {
+            guard let indexPathsForVisibleRows = self.tableView.indexPathsForVisibleRows else {
+                return
+            }
+
+            for indexPath in indexPathsForVisibleRows {
+                let cell = self.tableView.cellForRow(at: indexPath) as? CurrencyListTableViewCell
+                if cell?.currencyCodeLabel.text == self.viewModel.selectedCurrencyCode {
+                    continue
+                }
+
+                if let currency = self.viewModel.currency(with: cell?.currencyCodeLabel.text) {
+                    cell?.updateCell(amount: currency.amount)
+                }
+            }
+        }
     }
 }
--- CurrencyConverter/Features/CurrencyList/ViewModels/CurrencyListViewModel.swift
+++ CurrencyConverter/Features/CurrencyList/ViewModels/CurrencyListViewModel.swift
@@ -2,16 +2,204 @@
 //  CurrencyListViewModel.swift
 //  CurrencyConverter
 //
-//  Created by Abdullah Bayraktar on 02/12/2018.
-//  Copyright (c) 2018 Commencis. All rights reserved.
+//  Created by Abdullah Bayraktar on 03/12/2018.
+//  Copyright © 2018 AB. All rights reserved.
 //
-//  Save to the extent permitted by law, you may not use, copy, modify,
-//  distribute or create derivative works of this material or any part
-//  of it without the prior written consent of Commencis.
-//  Any reproduction of this material must contain this notice.
-//
+
+typealias CurrencyListFetchCompletion = (Bool) -> Void
+
+final class CurrencyListState {
+
+    enum Change {
+        case initialFetch(Bool)
+        case amountsUpdate(Bool)
+    }
+
+    /// Triggered when change occured
+    var onChange: ((CurrencyListState.Change) -> Void)?
+
+    /// Initial fetch state
+    fileprivate(set) var isInitialFetch: Bool = false {
+        didSet { onChange?(.initialFetch(isInitialFetch)) }
+    }
+
+    /// Amounts update state
+    fileprivate(set) var isUpdating: Bool = false {
+        didSet { onChange?(.amountsUpdate(isUpdating)) }
+    }
+}
 
 final class CurrencyListViewModel {
 
-    // TODO: To be implemented
+    /// State
+    private let state = CurrencyListState()
+
+    /// State change handler to trigger
+    var stateChangeHandler: ((CurrencyListState.Change) -> Void)? {
+        get { return state.onChange }
+        set { state.onChange = newValue }
+    }
+
+    // Data provider
+    private var dataProvider: CurrenciesDataProvider
+
+    /// Count of the currencies in currency list
+    var currenciesCount: Int {
+        return currencies?.count ?? 0
+    }
+
+    /// Amount entered by user
+    var enteredAmount: Double = 0
+
+    /// Currency code for the selected item
+    var selectedCurrencyCode: String = "EUR"
+
+    /// List of currencies
+    var currencies: [Currency]?
+
+    /// Initializes a new view model
+    ///
+    /// - Parameter dataProvider: Provided data provider
+    init(with dataProvider: CurrenciesDataProvider) {
+        self.dataProvider = dataProvider
+    }
+}
+
+// MARK: - Currency accessers
+
+extension CurrencyListViewModel {
+
+    /// Returns currency at given index
+    ///
+    /// - Parameter index: Index of the requested currency
+    /// - Returns: Currency if exists
+    func currency(at index: Int) -> Currency? {
+        guard index < currenciesCount,
+            let currencies = currencies else { return nil }
+
+        return currencies[index]
+    }
+
+    /// Returns currency with currency code
+    ///
+    /// - Parameter code: currency code
+    /// - Returns: Currency if exists
+    func currency(with code: String?) -> Currency? {
+        guard let code = code,
+            let currencies = currencies else { return nil }
+
+        for currency in currencies {
+            if code == currency.code {
+                return currency
+            }
+        }
+        return nil
+    }
+}
+
+// MARK: - Update Amounts
+
+extension CurrencyListViewModel {
+
+    /// Updates amounts for other currencies for given amount
+    ///
+    /// - Parameters
+    ///   - amount: Amount
+    ///   - currencyCode: Currency code
+    func updateAmount(amount: String?, currencyCode: String) {
+        if let amount = amount {
+            enteredAmount = Double(amount) ?? 0
+            calculateConvertedAmountsForEnteredAmount()
+        }
+    }
+
+    /// Updates selected currency and amount
+    ///
+    /// - Parameters
+    ///   - amount: Amount
+    ///   - currencyCode: Currency code
+    func updateSelectedCurrency(amount: String?, currencyCode: String) {
+        if let amount = amount {
+            enteredAmount = Double(amount) ?? 0
+            selectedCurrencyCode = currencyCode
+        }
+    }
+
+    /// Moves current item to the top of currenices list
+    ///
+    /// - Parameter index: Index for the selected cell
+    func moveToTopOfCurrencies(from index: Int) {
+
+        var currentCurrencies = currencies ?? []
+        let element = currentCurrencies.remove(at: index)
+        currentCurrencies.insert(element, at: 0)
+        currencies = currentCurrencies
+
+        fetchCurrencyRates(isInitialFetch: false)
+    }
+}
+
+// MARK: - Amount Calculations
+
+private extension CurrencyListViewModel {
+
+    /// Calculates amounts
+    func calculateConvertedAmountsForEnteredAmount() {
+        guard let currencies = currencies else { return }
+
+        var updatedAmounts: [Currency] = []
+        for currency in currencies {
+            let amountDouble = enteredAmount * (currency.rate ?? 0)
+            currency.amount = amountDouble == 0 ? "" : String(format: "%.2f", amountDouble)
+            updatedAmounts.append(currency)
+        }
+
+        self.currencies = updatedAmounts
+        state.isUpdating = false
+    }
+}
+
+// MARK: - Network
+
+extension CurrencyListViewModel {
+
+    /// Fetch currencies along with exchange rates
+    ///
+    /// - Parameter isInitialFetch: Flag to determine if it is initial fetch
+    func fetchCurrencyRates(isInitialFetch: Bool) {
+        let baseCurrency = selectedCurrencyCode
+
+        state.isUpdating = true
+        dataProvider.retrieveExchangeRates(
+            baseCurrency: baseCurrency, completion: { [weak self] (response, error) in
+
+                guard let strongSelf = self else { return }
+
+                if error == nil,
+                    let exchangeRatesDictionary = response?.exchangeRates {
+
+                    if isInitialFetch {
+                        var retrievedCurrencies: [Currency] = []
+                        for (key, value) in exchangeRatesDictionary {
+                            let currency = Currency(code: key, amount: nil, rate: value)
+                            retrievedCurrencies.append(currency)
+                        }
+                        // Add base currency
+                        let baseCurrency = Currency(code: baseCurrency, amount: nil, rate: nil)
+                        retrievedCurrencies.insert(baseCurrency, at: 0)
+
+                        strongSelf.currencies = retrievedCurrencies
+                    } else {
+                        for (key, value) in exchangeRatesDictionary {
+                            strongSelf.currencies?.filter({$0.code == key}).forEach {
+                                $0.rate = value
+                            }
+                        }
+                    }
+
+                    strongSelf.calculateConvertedAmountsForEnteredAmount()
+                    strongSelf.state.isInitialFetch = isInitialFetch
+                }
+        })
+    }
 }
